2511a79bc97f5e2d4ef014fa6884f1564c2baf8c
pabgarbar <ezdes23@hotmail.com>
2020-11-11 17:43:37 +0100

    added images

--- street_network/main/cars.js
+++ street_network/main/cars.js
@@ -8,7 +8,7 @@ const { timeEnd } = require('console');
 let velocities = [];
 
 
-/*const argv = yargs
+const argv2 = yargs
     .command('generateCars', 'Generate detections during the given time', {
         minFlow: {
             description: 'minimum cars per second',
@@ -28,9 +28,10 @@ let velocities = [];
       }
     )
 .help().alias('help', 'h').argv;
-if (argv._.includes('generateCars')) {
 
-    let carsPerSecond = (argv.minFlow+argv.maxFlow)/2;
+if (argv2._.includes('generateCars')) {
+
+    let carsPerSecond = (argv2.minFlow+argv2.maxFlow)/2;
 
     let csvBody = "VELOCITY,TIME_START\n";
 
@@ -38,7 +39,7 @@ if (argv._.includes('generateCars')) {
         carsPerSecond = 6;
     }
 
-    let numberCars = parseInt(carsPerSecond*(argv.time+72));
+    let numberCars = parseInt(carsPerSecond*(argv2.time+72));
     console.log(numberCars)
 
     while (velocities.length < numberCars) {
@@ -52,8 +53,8 @@ if (argv._.includes('generateCars')) {
     for(i = 0; i < numberCars; i++){
         csvBody += velocities[i] + "," + (((i/numberCars) * numberCars*1000) - 72000).toFixed(0)+"\n";
     }
-    fs.writeFileSync('./cars3.csv', csvBody,'utf8');
-}*/
+    fs.writeFileSync('./cars6.csv', csvBody,'utf8');
+}
 
 const argv = yargs
     .command('generateCarsVariableDensity', 'Generate detections during the given time', {
@@ -109,50 +110,3 @@ if (argv._.includes('generateCarsVariableDensity')) {
     }
     fs.writeFileSync('./cars6.csv', csvBody,'utf8');
 }
-
-/*let interval = setInterval(() => {
-
-    
-    console.log(inde.filter((i) => {
-         return (velocities[i] * (Date.now() - initialTime - timeStart[i])/3600000) >= (1*1)/3 &&
-          (velocities[i] * (Date.now() - initialTime - 1000 - timeStart[i])/3600000) < (1*1)/3;
-     }).length);
-}, 1000);
-
-setTimeout(() => {
-    clearInterval(interval);
-}, 1*60000 + 100);
-/*arra = [50, 60, 70, 30, 30];
-console.log(arra.filter((number) => {
-    return number > 50;
-}).length);
-console.log(arra.indexOf(30));
-let csvBody = "";
-fs.readFile('./cars.csv', (err, data) => {
-    if(err){
-    }else{
-        csvBody = data.toString();
-    }
-});
-setTimeout(() => {
-    for(i=0; i<75; i++){
-        csvBody += ((Math.random()*50+20).toFixed(0) +"," + (Math.random()*106000+60000).toFixed(0) + "\n");
-    
-    }
-}, 1000);
-
-setTimeout(() => {
-    fs.writeFileSync('./cars.csv', csvBody,'utf8');
-  
-}, 3000);*/
-
-
-/*request('http://localhost:9090/api/v1/query?query=ledger_blockchain_height&time=1594288905', function (error, response, body) {
-    if(!error){
-        console.log(JSON.parse(body).data.result);
-    } else {
-        console.log(error);
-    }
-});*/
-
-console.log(parseInt(2.3))
