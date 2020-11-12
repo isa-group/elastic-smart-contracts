ce12d8bacf87426defb6c7b731a781d7dcfd98ba
pabgarbar <ezdes23@hotmail.com>
2020-11-12 20:55:35 +0100

    reorganized the scripts

--- street_network/ExperimentSetElasticity/launchExperimentsElastic.sh
+++ street_network/ExperimentSetElasticity/launchExperimentsElastic.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+if [ "$#" -ne 9 ]; then
+    echo "ERROR: Wrong number of parameters!"
+    echo "Usage: $0 <number of sensors> <execution time in seconds> <calculation frequency in seconds> <sensor update frequency in seconds> <time window in seconds> <evaluate elasticity every X calculation> <street length in kilometers> <maximum calculation time allowed in miliseconds> <minimum calculation time allowed in miliseconds>"
+    echo "Example: $0 4 3600 8 1 1800 5 1 100 50"
+    exit
+fi
+
 echo "########### LAUNCHING THE EXPERIMENTS ###########"
-./testFrequency.sh
+./testFrequency.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
 sleep 10
-./testNoElasticity.sh
+./testNoElasticity.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
 sleep 10
-./testTimeWindow.sh
+./testTimeWindow.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
  echo "############ SCRIPT FINISHED ############"
--- street_network/ExperimentSetElasticity/testTimeWindow.sh
+++ street_network/ExperimentSetElasticity/testTimeWindow.sh
@@ -10,12 +10,12 @@ docker network connect net_test $(docker ps -a -q --filter ancestor=prom/prometh
 cd main
 sleep 1
 ./register.sh 4
-node listenerV2.js launchListener -n 4 -m 3600 -f 8 -t 1800 -p testWindow_2 --maxc 100 --minc 50 -s 1 &
+node listenerV2.js launchListener -n $1 -m $2 -f $3 -t $5 -p testTimeWindow --maxc $8 --minc $9 -s $7 &
 WR=$!
 sleep 1
 for i in $(seq 1 1 4)
 do
-    node invokeV2.js launchDetections -n 4 -m 3600 -s 1 -j $i -t 1800 -d 1 --maxc 100 --minc 50 --fcc 5 -e 2 &
+    node invokeV2.js launchDetections -n $1 -m $2 -s $7 -j $i -t $5 -d $4 --maxc $8 --minc $9 --fcc $6 -e 2 &
 done
 
 wait $WR
