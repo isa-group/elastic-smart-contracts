7df2be6db5391410bad3921776dae63e7d8cbb8a
jesusaparicioortiz <jesapaort@alum.us.es>
2022-09-12 17:37:57 +0200

    Removing files related to galibo

--- experiments/experiment32.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X POST "http://localhost:5900/api/v1/shutdown" -s -o /dev/null
-curl -X POST "http://localhost:5900/api/v1/startup" -s -o /dev/null
-sleep 5
-curl "http://localhost:5400/api/v6/setUpAccountableRegistry/oti_gc_ans1"
-sleep 900
-curl -X DELETE "http://localhost:5900/api/v1/stop/oti_gc_ans1"
